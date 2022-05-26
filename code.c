01c61745d9cc7e8e42cc8c0d07521088178fa1af
JoaoCardoso00 <jvcq13@gmail.com>
2022-05-26 14:42:49 -0300

    added arduino code

---
+++ code.c
@@ -0,0 +1,23 @@
+#include <Servo.h> //INCLUSÃO DA BIBLIOTECA NECESSÁRIA
+ 
+#define pinoServo 10 //PINO DIGITAL UTILIZADO PELO SERVO  
+ 
+Servo s; //OBJETO DO TIPO SERVO
+int pos; //POSIÇÃO DO SERVO
+ 
+void setup (){
+  s.attach(pinoServo); //ASSOCIAÇÃO DO PINO DIGITAL AO OBJETO DO TIPO SERVO
+  s.write(0); //INICIA O MOTOR NA POSIÇÃO 0º
+}
+void loop(){
+  for(pos = 0; pos < 180; pos++){ //PARA "pos" IGUAL A 0, ENQUANTO "pos" MENOR QUE 180, INCREMENTA "pos"
+    s.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
+    delay(5);
+  }
+  delay(2000); //INTERVALO DE 2 SEGUNDO
+  for(pos = 180; pos >= 0; pos--){ //PARA "pos" IGUAL A 180, ENQUANTO "pos" MAIOR OU IGUAL QUE 0, DECREMENTA "pos"
+    s.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
+    delay(5);
+  }
+  delay(2000);
+}
